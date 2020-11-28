9bc6429a99eff76f2b068b2dba135a9d221e1eea
jerryw2005 <56736925+jerryw2005@users.noreply.github.com>
2020-11-28 14:03:09 +0100

    You a little nigger

---
+++ kortsluiting-project/mainpage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request, 'mainpage/home.html')
+
+def tickets(request):
+    return render(request, 'mainpage/tickets.html')
+
+def about(request):
+    return render(request, 'mainpage/about.html')
